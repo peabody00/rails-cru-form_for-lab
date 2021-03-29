e193330569c5246756ab76a450821fe241af669c
Charlie <peabody00@gmail.com>
2021-03-29 14:07:37 -0500

    commit tree bcd48fc9c4b6a347b710444973751cf815233b60

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,38 @@
+class ArtistsController < ApplicationController
+    before_action :set_artist, only: [:show, :edit, :update]
+  
+    def index
+      @artists = Artist.all  
+    end 
+  
+    def show
+    end
+  
+    def new
+      @artist = Artist.new
+    end
+  
+    def edit
+    end
+  
+    def create
+      @artist = Artist.new(artist_params)
+      @artist.save
+      redirect_to artist_path(@artist) 
+    end 
+  
+    def update
+      @artist.update(artist_params)
+      redirect_to artist_path(@artist)
+    end 
+  
+    private
+  
+    def artist_params
+      params.require(:artist).permit(:name, :bio)
+    end 
+  
+    def set_artist
+      @artist = Artist.find(params[:id]) 
+    end 
+  end
